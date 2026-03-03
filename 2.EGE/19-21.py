0339e2c7a211431fc9093539bf3ffe20621e4263
HrapD <pik722326@gmail.com>
2026-03-03 21:04:28 +0700

    03.03.2026 Number 19-21 practice

--- 2.EGE/19-21.py
+++ 2.EGE/19-21.py
@@ -103,11 +103,79 @@ print([s for s in range(23, 1000) if F(50, s, n=3) and not F(50, s, n=1)])
 print([s for s in range(23, 1000) if F(50, s, n=4) and not F(50, s, n=2)])
 """
 
-'''
-Запустите бота: https://t.me/ilandroxxy_bot и нажмите кнопку: "📚 Получить конспект"
-'''
-# endregion Место для вашего конспекта ⬆️
+#                         03.03.2026
+#__________________________________________________________________________________
 
-# Домашка 19-21 номер: https://stepik.org/lesson/1038794/step/1?unit=1062789
+#№ 25358 ЕГКР 13.12.25 (Уровень: Базовый) 1 куча увеличение
+"""def F(s, n):
+    if s >= 125:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s + 2, n - 1), F(s + 4, n - 1), F(s * 2, n -1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+print ([s for s in range (1, 124+1) if F(s, n = 2)])
+print ([s for s in range (1, 124+1) if F(s, n = 3) and not F(s, n = 1)])
+print ([s for s in range (1, 124+1) if F(s, n = 4) and not F(s, n = 2)])"""
 
-# Практика 19-21 номер: https://stepik.org/lesson/1221217/step/1?unit=1234627
+#№ 23759 Демоверсия 2026 (Уровень: Базовый) 1 куча вниз
+"""# -3, -5, /4,|s <= 30|s >= 31|
+from math import ceil, floor
+def F(s, n):
+    if s <= 30:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s - 3, n - 1), F(s - 5, n -1), F (floor(s / 4), n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+print ([s for s in range(31, 1000) if F(s, n = 2)])
+print ([s for s in range(31, 1000) if F(s, n = 3) and not F(s, n = 1)])
+print ([s for s in range(31, 1000) if F(s, n = 4) and not F(s, n = 2)])"""
+
+#№ 23565 Пересдача 03.07.25 (Уровень: Базовый) 1 уча вниз
+"""# -3, -8, /3, |s <= 15| s >=16|
+from math import ceil, floor
+def F(s, n):
+    if s <= 15:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s - 3, n - 1), F(s -8, n - 1), F(floor(s / 3), n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+print ([s for s in range(16, 1000) if F(s, n = 2)])
+print ([s for s in range(16, 1000) if F(s, n = 3) and not F(s, n = 1)])
+print ([s for s in range(16, 1000) if F(s, n = 4) and not F(s, n = 2)])
+"""
+
+#№ 23378 Резервный день 19.06.25 (Уровень: Базовый) 1 куча вниз
+"""#   -6, -3 /3| S <= 27|S >= 28|
+from math import ceil, floor
+def F(s, n):
+    if s <= 27:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s - 3, n - 1), F(s - 6, n - 1), F(floor(s / 3), n -1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+print ([s for s in range(28, 1000) if F(s, n = 2)])
+print ([s for s in range(28, 1000) if F(s, n = 3) and not F(s, n = 1)])
+print ([s for s in range(28, 1000) if F(s, n = 4) and not F(s, n = 2)])"""
+
+#№ 21905 Открытый вариант 2025 (Уровень: Базовый) 1 куча вверх
+"""# +1 +4 *3 | s <= 65 | 1 ≤ S ≤ 66 |
+def F(s, n):
+    if s >= 67:
+        return n % 2 == 0
+    if n == 0:
+        return 0
+    h = [F(s + 1, n - 1), F(s + 4, n - 1), F(s * 3, n - 1)]
+    return any(h) if (n - 1) % 2 == 0 else all(h)
+print ([s for s in range (1, 66 + 1) if F(s, n = 2)])
+print ([s for s in range (1, 66 + 1) if F(s, n = 3) and not F(s, n = 1)])
+print ([s for s in range (1, 66 + 1) if F(s, n = 4) and not F(s, n = 2)])"""
+
+
+
+
+
+
--- 2.EGE/27.py
+++ 2.EGE/27.py
@@ -2,12 +2,42 @@
 # 📘 Полная версия шпаргалки доступна в нашем тг канале: https://t.me/informatika_kege_itpy/362?comment=6532
 # 📘 Разборы 27 номеров по информатике: https://t.me/informatika_kege_itpy/360?comment=7007
 
-# region Место для вашего конспекта ⬇️
-'''
-Запустите бота: https://t.me/ilandroxxy_bot и нажмите кнопку: "📚 Получить конспект"
-'''
-# endregion Место для вашего конспекта ⬆️
+#№ 23209 Основная волна 10.06.25 (Уровень: Базовый)
+'''from math import dist
+def center(cl):
+    R = []
+    for p in cl:
+        summa = 0
+        for g in cl:
+            summa += dist(p, g)
+        R.append([summa, p])
+    return min(R)[1]
+clusterA = [[], []]
+clusterB = [[], [], []]
+for s in open('27_A_23209.txt'):
+    s = s.replace(',', '.')
+    x, y = [float(x) for x in s.split()]
+    if x > 5:
+        clusterA[0].append([x, y])
+    if x < 5:
+        clusterA[1].append([x, y])
+for s in open('27_B_23209.txt'):
+    s = s.replace(',', '.')
+    x, y = [float(x) for x in s.split()]
+    if y > 21 and x > 6 and x < 16:
+        clusterB[0].append([x, y])
+    if y < 21 and y > 12 and x > 6 and x < 16:
+        clusterB[1].append([x, y])
+    if y < 12 and x > 6 and x < 16:
+        clusterB[2].append([x, y])
+print(center(clusterA[0]))#[6.9663606, 19.2156207]
+print(center(clusterA[1]))#[3.9100466, 6.6396418]
+PxMAX = int(6.9663606 * 10000)
+PyMAX = int(19.2156207 * 10000)
+print(center(clusterB[0]), len(clusterB[0]))#[12.1302947, 23.4222296] 75
+print(center(clusterB[1]), len(clusterB[1]))#[8.4874654, 18.9031256] 131
+print(center(clusterB[2]), len(clusterB[2]))#[12.2170442, 7.2915548] 397
+Qx = int(abs((12.1302947 - 12.2170442) * 10000))
+Qy = int(abs((23.4222296 - 7.2915548) * 10000))
+print(PxMAX, PyMAX, Qx, Qy)#Answer: 69663 192156 867 161306'''
 
-# Домашка 27 номер: https://stepik.org/lesson/1400061/step/1?unit=1417014
-
-# Практика 27 номер: https://stepik.org/lesson/1400062/step/1?unit=1417015
